f8f2d5cea1c41d489c05bbf754ba33661843779f
leonardoalmeida7 <leonardoalmeida2124@gmail.com>
2024-11-09 23:32:19 -0300

    corrigindo bugs

--- 005-Article-preview/script.js
+++ 005-Article-preview/script.js
@@ -1,8 +1,10 @@
 let share = document.getElementsByClassName("share")[0]
 let icons = document.querySelectorAll(".icon-share")
+let profile = document.getElementsByClassName("profile")[0]
 
 icons.forEach(icon => {
     icon.addEventListener("click", function(){
         share.classList.toggle("show")
+        profile.classList.toggle("hidden")
     })
 })
