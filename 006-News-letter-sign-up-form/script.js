e778996366a8c493f561d3a5cc166182694337d1
leonardoalmeida7 <leonardoalmeida2124@gmail.com>
2024-11-14 21:31:41 -0300

    new project

---
+++ 006-News-letter-sign-up-form/script.js
@@ -0,0 +1,17 @@
+let input_email = document.getElementById("email")
+let text_erro = document.getElementById("text-erro")
+let button = document.getElementById("button")
+
+button.addEventListener("click", function(){
+    if(input_email.value.includes("@")){
+        input_email.style.backgroundColor = "#8ef87959"
+        input_email.style.borderColor = "#8ef879"
+        setTimeout(() => {
+            window.location.href = "/success_page.html"
+        }, 1500)
+    }else {
+        input_email.style.backgroundColor = "#f87d796b"
+        input_email.style.borderColor = "#f87d79"
+        text_erro.innerHTML = "Valid email required"
+    }
+})
