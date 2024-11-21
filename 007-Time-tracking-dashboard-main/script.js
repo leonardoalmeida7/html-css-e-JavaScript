3c9e6cb88ae6bba15abcc2d1cccbf065582289ad
leonardoalmeida7 <leonardoalmeida2124@gmail.com>
2024-11-20 21:39:46 -0300

    javascript

---
+++ 007-Time-tracking-dashboard-main/script.js
@@ -0,0 +1,65 @@
+let daily = document.getElementsByTagName('li')[0]
+let weekly = document.getElementsByTagName('li')[1]
+let monthly = document.getElementsByTagName('li')[2]
+let daily_info = document.querySelectorAll('.daily')
+let weekly_info = document.querySelectorAll('.weekly')
+let monthly_info = document.querySelectorAll('.monthly')
+
+daily.classList = "active"
+weekly_info.forEach(info => {
+    info.classList = "hidden"
+})
+monthly_info.forEach(info => {
+    info.classList = "hidden"
+})
+
+daily.addEventListener("click", function(){
+    daily.classList = "active"
+    weekly.classList.remove("active")
+    monthly.classList.remove("active")
+    daily_info.forEach(info => {
+        info.classList.remove("hidden")
+    })
+    weekly_info.forEach(info => {
+        info.classList = "hidden"
+    })
+    monthly_info.forEach(info => {
+        info.classList = "hidden"
+    })
+    
+})
+
+weekly.addEventListener("click", function(){
+    weekly.classList = "active"
+    monthly.classList.remove("active")
+    daily.classList.remove("active")
+    
+    weekly_info.forEach(info => {
+        info.classList.remove('hidden')
+    })
+    daily_info.forEach(info => {
+        info.classList = "hidden"
+    })
+    monthly_info.forEach(info => {
+        info.classList = "hidden"
+    })
+})
+
+monthly.addEventListener("click", function(){
+    monthly.classList = "active"
+    daily.classList.remove("active")
+    weekly.classList.remove("active")
+
+    monthly_info.forEach(info => {
+        info.classList.remove('hidden')
+    })
+
+    daily_info.forEach(info => {
+        info.classList = "hidden"
+    })
+    weekly_info.forEach(info => {
+        info.classList = "hidden"
+    })
+})
+
+
