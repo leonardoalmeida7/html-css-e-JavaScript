b27f32b51814286cc0a8f1ca543fc1d00d9cb211
leonardoalmeida7 <leonardoalmeida2124@gmail.com>
2025-04-05 14:02:31 -0300

    finalizado

--- 027-product-list-with-cart-main/assets/js/script.js
+++ 027-product-list-with-cart-main/assets/js/script.js
@@ -6,7 +6,7 @@ const buttonController = document.querySelectorAll('.quantity-control');
 const itemsCart = document.querySelector('.itens-cart');
 const countQuantityItems = document.querySelector('.quantity');
 const buttonConfirm = document.querySelector('.confirm');
-
+const containerItems = document.querySelectorAll('.img-item');
 
 
 fetch("data.json")
@@ -43,13 +43,14 @@ fetch("data.json")
     .catch(error => console.error("Erro ao carregar o JSON", error));
 
 
-
 function getItemsInfos(num){
     let getItems = localStorage.getItem('items');
     let objItems = JSON.parse(getItems);
-    let infoItem = [objItems[num].name, objItems[num].category, objItems[num].price];
+    let infoItem = {name: objItems[num].name, cartegory: objItems[num].category, price: objItems[num].price};
     return infoItem;
 }
+
+
 
 let idButton = 0;
 buttonsCart.forEach(button => {
@@ -82,9 +83,11 @@ function buttonToggle(){
                     buttonController[idDecrement].classList.add('hidden');
                     buttonsCart[idDecrement].classList.remove('hidden');
                     buttonController[idDecrement].classList.remove('hidden');
-                    numItems[idDecrement].textContent = 0;
+                    //numItems[idDecrement].textContent = 0;
+                    containerItems[idDecrement].style.border = 'none';
                     removeItemCart(idDecrement);
                     calculateItemsCart();
+                    calculateTotalPrice();
                 }
             }
         })
@@ -118,10 +121,10 @@ function addToCart(infoItem){
     
     divElement.id = infoItem;
     
-    itemName.textContent = infos[0];
+    itemName.textContent = infos.name;
     spanElement.textContent = '1x';
-    spanElement2.textContent = '@ $' + infos[2].toFixed(2);
-    spanElement3.textContent = '$' + infos[2].toFixed(2);
+    spanElement2.textContent = '@ $' + infos.price.toFixed(2);
+    spanElement3.textContent = '$' + infos.price.toFixed(2);
     divElementChild2.textContent = 'x';
     
     itemsCart.appendChild(divElement);
@@ -134,9 +137,8 @@ function addToCart(infoItem){
     divElementChild.appendChild(spanElement3);
 
     
-    calculateTotalPrice()
+    calculateTotalPrice();
 }
-
 
 function calculateItems(id){
     let listCart = [];
@@ -149,7 +151,7 @@ function calculateItems(id){
     }
     let item = listCart.indexOf('' + id);
     listItemsQnd[item].textContent = numItems[id].textContent + 'x';
-    listItemsQndTotal[item].textContent = '$' + (getItemsInfos(id)[2] * numItems[id].textContent).toFixed(2);
+    listItemsQndTotal[item].textContent = '$' + (getItemsInfos(id).price * numItems[id].textContent).toFixed(2);
 
     calculateTotalPrice();
     calculateItemsCart();
@@ -171,7 +173,7 @@ function calculateTotalPrice(){
     let test = [];
     priceItemTotal.forEach((price) => {
         test.push(price.textContent.substring(1));
-        orderTotal.textContent = Number(test.reduce((v, t) => Number(v) + Number(t))).toFixed(2);
+        orderTotal.textContent = '$' + Number(test.reduce((v, t) => Number(v) + Number(t))).toFixed(2);
         
     })
     
@@ -181,12 +183,11 @@ function calculateTotalPrice(){
 
 function removeItemCart(id){
     let listItems = document.querySelectorAll('.item-cart');
-    
+    containerItems[id].style.border = 'none';
 
    listItems.forEach((item, index) => {
         if (item.id == id) {
             item.remove();
-
         }
     })
 
@@ -194,8 +195,64 @@ function removeItemCart(id){
     buttonsCart[id].classList.remove('hidden');
     numItems[id].textContent = 0;
     calculateItemsCart();
+    calculateTotalPrice();
+}
+
+function getImageThumbnail(num){
+    let getImage = localStorage.getItem('items');
+    let objItems = JSON.parse(getImage);
+    let linkImage = [objItems[num].image.thumbnail];
+    return linkImage;
 }
 
 function openModal(){
+    const modal = document.querySelector('.cart-modal');
+    modal.classList.remove('hidden');
+    const modalItems = document.querySelector('.itens-cart-modal');
+    const itemCart = document.querySelectorAll('.item-cart');
+    let listItemsQnd = document.querySelectorAll('.num-item-cart');
+    let listItemsQndTotal = document.querySelectorAll('.price-item-total');
+    let priceTotal = document.querySelector('.price-total');
+    itemCart.forEach((item, index) => {
+        const divItem = document.createElement('div');
+        divItem.setAttribute('class', 'item-cart-modal row px-3 align-items-center');
+        const divContainerImage = document.createElement('div');
+        divContainerImage.setAttribute('class', 'container-image-thumbnail col-3');
+        const imageThumbnail = document.createElement('img');
+        imageThumbnail.setAttribute('src', getImageThumbnail(item.id));
+        const divContainerInfos = document.createElement('div');
+        divContainerInfos.setAttribute('class', 'col-6')
+        const nameElement = document.createElement('p');
+        nameElement.setAttribute('class', 'name-item-cart');
+        const quantityElement = document.createElement('span');
+        quantityElement.setAttribute('class', 'num-item-cart me-3');
+        const priceElement = document.createElement('span');
+        priceElement.setAttribute('class','price-item-cart me-3');
+        const divContainerTotalPrice = document.createElement('div');
+        divContainerTotalPrice.setAttribute('class', 'container-price-total col-3');
+        const sumTotalPrice = document.createElement('span');
+        sumTotalPrice.setAttribute('class', 'price-item-total-modal fs-5 fw-bold');
 
+        nameElement.textContent = getItemsInfos(item.id).name;
+        quantityElement.textContent = listItemsQnd[index].textContent;
+        priceElement.textContent = '@ $' + (getItemsInfos(item.id).price).toFixed(2);
+        sumTotalPrice.textContent = listItemsQndTotal[index].textContent;
+
+        divContainerImage.appendChild(imageThumbnail);
+        divContainerInfos.appendChild(nameElement);
+        divContainerInfos.appendChild(quantityElement);
+        divContainerInfos.appendChild(priceElement);
+        divContainerTotalPrice.appendChild(sumTotalPrice);
+        divItem.appendChild(divContainerImage);
+        divItem.appendChild(divContainerInfos);
+        divItem.appendChild(divContainerTotalPrice);
+        modalItems.appendChild(divItem);
+    })
+
+    document.querySelector('.price-total-modal').textContent = priceTotal.textContent;
+
+    const newOrder = document.querySelector('#new-order');
+    newOrder.addEventListener('click', () => {
+        window.location.reload();
+    })
 }
